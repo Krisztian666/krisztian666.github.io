3ddb94f306f4b4beff12fdaac718d35431cda0be
jkqxl2 <krisztian.karoczkai@sztaki.hu>
2015-08-12 16:13:21 +0200

    worker

--- webmaker/examples/worker/runningforever.js
+++ webmaker/examples/worker/runningforever.js
@@ -1,5 +1,5 @@
 function runningforever(){ 
     postMessage(Math.random()); 
-    setTimeout("timedCount()",Math.floor((Math.random()*1000)+1)); 
+    setTimeout(runningforever,Math.floor((Math.random()*1000)+1)); 
 } 
 runningforever();
--- webmaker/runningforever.js
+++
@@ -1,5 +0,0 @@
-function runningforever(){ 
-    postMessage(Math.random()); 
-    setTimeout("timedCount()",Math.floor((Math.random()*1000)+1)); 
-} 
-runningforever();
